5768795ba7fbaffaeaa7ea49a10153adcfc13221
kdong <khoidong.ca@gmail.com>
2024-05-02 13:37:39 -0500

    add loop

--- chapter2/sanbox.js
+++ chapter2/sanbox.js
@@ -1,2 +1,5 @@
 let age = 23;
 console.log(age);
+
+console.log(age == 23);
+console.log(age != 23);
---
+++ chapter3/sandbox.js
@@ -0,0 +1,18 @@
+//for loop
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("in loop", i);
+// }
+
+// console.log("loop end");
+
+const names = ["shaun", "mario", "luigi"];
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+let i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
